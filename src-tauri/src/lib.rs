5bf82cd0937edff2b330c0ea244116836a42b072
juheeNam <157343174+juheeNam@users.noreply.github.com>
2026-05-11 09:28:13 +0900

    feat: 인앱 자동 업데이트 나머지 파일 적용

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,6 +16,8 @@ fn quit(app: tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_notification::init())
